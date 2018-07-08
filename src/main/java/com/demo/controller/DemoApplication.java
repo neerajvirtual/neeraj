6147c53a828b52c88c81b5c544fe3809064dbebf
akash0380 <akash0380>
2018-07-09 01:26:35 +0530

    add patch to run on server

--- src/main/java/com/demo/controller/DemoApplication.java
+++ src/main/java/com/demo/controller/DemoApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,12 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan({"com.demo"})
 @EnableJpaRepositories(basePackages = "com.demo.dao")
 @EntityScan(basePackages = "com.demo.model")
-public class DemoApplication extends SpringBootServletInitializer {
-	
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApplication.class);
-    }
+public class DemoApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
