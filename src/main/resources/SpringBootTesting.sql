6a7bfff0bb7eea17a184ab40618e515fda580f7f
akash0380 <akash0380@gmail.com>
2018-08-27 00:39:17 +0530

    added sql file

--- src/main/resources/SpringBootTesting.sql
+++ src/main/resources/SpringBootTesting.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
 --
 -- Host: localhost    Database: SpringBootTesting
 -- ------------------------------------------------------
--- Server version	5.7.22-0ubuntu0.16.04.1
+-- Server version	5.7.23-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,63 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `food`
+--
+
+DROP TABLE IF EXISTS `food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `food` (
+  `food_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `status` tinyint(1) DEFAULT '1',
+  `food_type_id` int(11) NOT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`food_id`),
+  KEY `FKf3bmvf487ya6cqglsh3lotiiq` (`food_type_id`),
+  KEY `FK1xxp5osqkx3gxmdfqsqwhm1fw` (`type_id`),
+  CONSTRAINT `FK1xxp5osqkx3gxmdfqsqwhm1fw` FOREIGN KEY (`type_id`) REFERENCES `food_type` (`food_type_id`),
+  CONSTRAINT `FKf3bmvf487ya6cqglsh3lotiiq` FOREIGN KEY (`food_type_id`) REFERENCES `food_type` (`food_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food`
+--
+
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (1,'MixVeg','MixVeg for dinner',1,1,1),(2,'Biryani','Biryani for all time',1,2,2);
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `food_type`
+--
+
+DROP TABLE IF EXISTS `food_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `food_type` (
+  `food_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`food_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food_type`
+--
+
+LOCK TABLES `food_type` WRITE;
+/*!40000 ALTER TABLE `food_type` DISABLE KEYS */;
+INSERT INTO `food_type` VALUES (1,'Veg',1),(2,'NonVeg',1);
+/*!40000 ALTER TABLE `food_type` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `status`
@@ -53,9 +110,16 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status_id` int(11) NOT NULL,
+  `food_type_id` int(11) NOT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK3m08uc0bd36m6tgp3g65m20dl` (`status_id`),
-  CONSTRAINT `FK3m08uc0bd36m6tgp3g65m20dl` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`)
+  KEY `FKgt7l6dw1xavs1cvgkfly4d2n1` (`food_type_id`),
+  CONSTRAINT `FK3m08uc0bd36m6tgp3g65m20dl` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`),
+  CONSTRAINT `FKgt7l6dw1xavs1cvgkfly4d2n1` FOREIGN KEY (`food_type_id`) REFERENCES `food_type` (`food_type_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -65,7 +129,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'dipti','dipti@gmail.com','12345',2),(2,'akash','akash@gmail.com','54321',1),(8,'akash','akash@gmail.com','54321',1);
+INSERT INTO `users` VALUES (1,'dipti','dipti@gmail.com','12345',1,1,1,'2018-07-31 18:30:00',1,'2018-07-31 18:30:00'),(2,'akash','akash@gmail.com','54321',1,2,1,'2018-07-31 18:30:00',1,'2018-07-31 18:30:00'),(8,'akshat','akshat@gmail.com','111111',1,1,1,'2018-07-31 18:30:00',1,'2018-07-31 18:30:00');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -78,4 +142,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-05 18:32:56
+-- Dump completed on 2018-08-27  0:38:47
